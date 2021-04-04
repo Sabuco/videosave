a341ebd63ab2a32ae3be7317ced51b563f276eaa
Alvaro García Sabuco <alvarogs56@gmail.com>
2021-04-04 13:45:31 +0200

    Maquetacion, componentes, rutas y paginas

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
+
+@Component({
+  selector: 'register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  public pageTitle: string;
+  public user: User;
+
+  constructor() {
+    this.pageTitle = "Registro";
+    this.user = new User(1, '', '', '', '', 'ROLE_USER', '', '');
+  }
+
+  ngOnInit(): void {
+    console.log(this.user);
+    
+  }
+
+  onSubmit(){
+    
+  }
+
+}
